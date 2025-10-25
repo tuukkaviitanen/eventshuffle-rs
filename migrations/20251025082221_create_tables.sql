eee1dce26af5e85b1ad587c919b40d99d69ecd5d
Tuukka Viitanen <tuukka.viitanen@gmail.com>
2025-10-25 12:08:56 +0300

    Tweaked the migration

--- migrations/20251025082221_create_tables.sql
+++ migrations/20251025082221_create_tables.sql
@@ -8,7 +8,7 @@ CREATE TABLE "Event" (
 CREATE TABLE "EventDate" (
     "id" CHAR(36) NOT NULL,
     "date" DATE NOT NULL,
-    "eventId" CHAR(36) NOT NULL,
+    "event_id" CHAR(36) NOT NULL,
 
     CONSTRAINT "EventDate_pkey" PRIMARY KEY ("id")
 );
@@ -16,11 +16,11 @@ CREATE TABLE "EventDate" (
 CREATE TABLE "EventDateVote" (
     "id" CHAR(36) NOT NULL,
     "name" VARCHAR(255) NOT NULL,
-    "eventDateId" CHAR(36) NOT NULL,
+    "event_date_id" CHAR(36) NOT NULL,
 
     CONSTRAINT "EventDateVote_pkey" PRIMARY KEY ("id")
 );
 
-ALTER TABLE "EventDate" ADD CONSTRAINT "EventDate_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "EventDate" ADD CONSTRAINT "EventDate_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "Event"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE "EventDateVote" ADD CONSTRAINT "EventDateVote_eventDateId_fkey" FOREIGN KEY ("eventDateId") REFERENCES "EventDate"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "EventDateVote" ADD CONSTRAINT "EventDateVote_event_date_id_fkey" FOREIGN KEY ("event_date_id") REFERENCES "EventDate"("id") ON DELETE CASCADE ON UPDATE CASCADE;
